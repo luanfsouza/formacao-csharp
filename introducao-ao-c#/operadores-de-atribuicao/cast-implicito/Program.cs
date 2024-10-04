8719c18bfda80f97778680fffb13364fbb25ee00
luanfsouza <luanferreira7777@gmail.com>
2024-10-03 23:09:17 -0300

    better folder organization

---
+++ introducao-ao-c#/operadores-de-atribuicao/cast-implicito/Program.cs
@@ -0,0 +1,5 @@
+﻿
+int a = 33;
+
+//implicitamente o a foi convertido para double, pq o inteiro cabe dento de um double
+double b = a;
--- operadores-de-atribuicao/cast-implicito/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿int a = 24;
-
-//implicitamente acontece b = Convert.ToDouble(a) || double.Parse(a)
-double b = a;
-
-int c = 4;
-long d = c;
